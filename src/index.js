1602a7ea4878aced85f679897cb463dfbaf4985b
AinuAnna <65246069+AinuAnna@users.noreply.github.com>
2020-09-30 23:35:47 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if ((array || []).length === 0)
+    return 0;
+  var min = array[0]; // min
 
-exports.max = function max (array) {
-  return 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
 }
-
-exports.avg = function avg (array) {
-  return 0;
+exports.max = function max(array) {
+  if ((array || []).length === 0)
+    return 0;
+  var max = array[0]; // max
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+} 
+exports.avg = function avg(array) {
+  if ((array || []).length === 0)
+    return 0;
+  var sum = array[0]; // sum
+  for (var i = 1; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum / array.length;
 }
